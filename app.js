ad95b5542861dc2a340195bf9b12858c2cb941f4
AzizullahCodes <azizullahcodes@gmail.com>
2025-06-23 22:17:35 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,56 @@
+// js-practice-assignment series no 34
+// _________________________________________________________________________________
+// Q 1 
+//  Ask the user to enter a number from 1 to 7 using prompt().
+// 2️⃣ Use a switch statement to show which day it is:
+// 1 → Sunday
+// 2 → Monday
+// 3 → Tuesday
+// 4 → Wednesday
+// 5 → Thursday
+// 6 → Friday
+// 7 → Saturday
+// 3️⃣ Show the day name using alert() and console.log().
+
+// _______________
+// ans 
+// let num = 77;
+// switch (num){case(1):console.log('sunday')
+//     break;
+//     case(2):console.log('Monday')
+//     break;
+//     case(3):console.log('Tuesday')
+//     break;
+//     case(4):console.log('Thursday')
+//     break;
+//     case(5):console.log('Friday')
+//     break;
+//     case(6):console.log('saturday');
+//     break;
+//     case(7):console.log('sunday');
+//     break;
+//     default:console.log('invalid entry')
+// }
+// ____________________________________________________________________________________
+// Q 2
+// ask the user to enter an operator: +, -, *, or /.
+// 2️⃣ Ask the user to enter two numbers.
+// 3️⃣ Use a switch statement to perform the calculation based on the operator.
+// 4️⃣ Show the result with alert() and console.log().
+//  let operator = prompt('enter an operator -,+,/ or *');
+//  let num1 = Number(prompt('enter number 1'));
+//  let num2 = Number(prompt('enter number 2'));
+//  switch (operator){case('+'):let sum = num1 + num2;
+//     console.log(sum)
+// break;
+// case('-'):let subtract = num1 - num2;
+// console.log(subtract);
+// break;
+// case('*'):let multiplication = num1 * num2;
+// console.log(multiplication);
+// break;
+// case('/'):let division = num1/ num2;
+// console.log(division);
+// break;
+// default:console.log('invalid entry')
+//  }
